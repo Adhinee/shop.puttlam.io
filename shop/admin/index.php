de68e16e32915193d9d3692e918158980c205cf8
Adhinee <103882598+Adhinee@users.noreply.github.com>
2023-06-28 01:04:32 +0530

    Add files via upload

---
+++ shop/admin/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+<body><div class="admin-nav">
+        <div class="logo">
+            <img src="../truck.png" width="125px">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Products</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                
+            </ul>
+        </nav>
+        <img src="../images/cart.png" width="25px" height="25px" >
+    </div>
+
+    <h2 style="text-align:center;">
+        Manage Details
+    </h2>
+    <div class="row" >
+        <div class="col">
+        <img src="../suger.png" width="100px">
+
+        <div class="butt">
+
+
+            <button>
+            <a href="" class="btn">
+                Insert Products
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                View Products
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                Insert Categories
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                View categories
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                Insert Brands
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                View Brands
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                All Orders
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                All Payments
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                List User
+                </a></button>
+            <button>
+                <a href="" class="btn">
+
+                Logout
+                </a></button>
+                
+        <p>
+            Admin
+        </P>
+    </div>
+    </div>
+    </div>
+    <div class="footer">
+    <p>All Rights Reserved @- Designed By Adhinee-2023</P>
+    </div>
+</body>
+</html>
